84ae87d90ffda568374f035c298b01da0a29def3
cho934 <christophe.chaudelet@gmail.com>
2022-02-07 08:04:00 +0100

    init projet

---
+++ INO_t41_PMXAxTeensy/INO_t41_PMXAxTeensy.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _INO_t41_PMXAxTeensy_H_
+#define _INO_t41_PMXAxTeensy_H_
+#include "Arduino.h"
+//add your includes for the project INO_t41_PMXAxTeensy here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project INO_t41_PMXAxTeensy here
+
+
+
+
+//Do not add code below this line
+#endif /* _INO_t41_PMXAxTeensy_H_ */
